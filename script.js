c916cb95431689ee4b3d46be2e5a7604b951cce1
Matthew Avallone <mva271@nyu.edu>
2021-01-09 22:20:51 -0500

    added Character class

--- script.js
+++ script.js
@@ -10,136 +10,119 @@ const ctx = canvas.getContext('2d');
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-// load img
-const  images = [];
-images.player = new Image();
-images.player.src = "characters/link/walking_master.png"; // link walking is 306x100
 
-
-images.player_right = new Image();
-images.player_right.src = "characters/link/walking_right.png";
-images.push(images.player_right);
-
-/*
-const characters = [];
-characters.push(new Character());
-
-//CLASS
-class Character { //create character class
-  constructor(){
-
+class Character {
+  constructor(width, height, frameX, frameY, x, y, speed, walkSource){
     //HERE write search for stat sheet based on type to set modifiers and sprite sheets
 
-    this.width = '36.875';
-    this.height = '31';
-    this.frameX = 0;
-    this.frameY = 0;
-    this.x = 200;
-    this.y = 200;
-    this.speed = (Math.random()*1.5 + 3); //random speed for now
+    this.width = width;
+    this.height = height;
+    this.frameX = frameX;
+    this.frameY = frameY;
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.walkSource = walkSource;
+    
+    // Load images for walking action
+    this.walking = []
+    let player = new Image();
+    player.src = this.walkSource;
+    this.walking.push(player)
+
+    if(this.walkSource.includes('link')){
+      let player_right = new Image();
+      player_right.src = "characters/link/walking_right.png";
+      this.walking.push(player_right);
+    }
   }
 
-  draw(){
-    drawSprite(images.player, this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height);
-  }
-}
-
-update(){
-
-  this.frameY = 2; //set down
-  if(this.y < canvas.height - this.height)
-    this.y += this.speed;
-    if(this.frameX<7)
-      this.frameX++;
-    else this.frameX = 0;
-  break
-}
-
-//CLASS*/
-
-const playerWidth = '36.4'; //8
-const playerHeight = '32'; //3
-let playerFrameX = 0; //sprite frame location
-let playerFrameY = 0;
-let playerX = 200; //player positions
-let playerY =100;
-const playerSpeed = 2;
-
-
-function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
+  draw(img, sX, sY, sW, sH, dX, dY, dW, dH) {
     ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
-}
-
-
-function animate_player(e){
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-
-  if(e == undefined){
-    return
   }
-  switch(e.code) {
+
+  animate(e){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    if(e.code == undefined){
+      this.draw(this.walking[0], this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height);
+      return
+    }
+
+    switch(e.code) {
         case "ArrowUp": //up
-          playerFrameY = 0; //set up facing link on link sprite sheet
+          this.frameY = 0; //set up facing link on link sprite sheet
 
-          if(playerY >= 0){ 
-            playerY -= playerSpeed;
-            if(playerFrameX<7) playerFrameX++;
-            else playerFrameX = 0;
+          if(this.y >= 0){ 
+            this.y -= this.speed;
+            if(this.frameX < 7) this.frameX++;
+            else this.frameX = 0;
           }
-          else playerY = 0;
+          else this.y = 0;
 
-          drawSprite(images.player, playerWidth * playerFrameX, playerHeight * playerFrameY, playerWidth, playerHeight, playerX, playerY, playerWidth, playerHeight);
+          this.draw(this.walking[0], this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height);
           break;
 
         case "ArrowDown": //down
-          playerFrameY = 2; //set down
+          this.frameY = 2; //set down
 
-          if(playerY < canvas.height - playerHeight)
-            playerY += playerSpeed;
-            if(playerFrameX<7)
-              playerFrameX++;
-            else playerFrameX = 0;
+          if(this.y < canvas.height - this.height)
+            this.y += this.speed;
+            if(this.frameX < 7)
+              this.frameX++;
+            else this.frameX = 0;
 
-          drawSprite(images.player, playerWidth * playerFrameX, playerHeight * playerFrameY, playerWidth, playerHeight, playerX, playerY, playerWidth, playerHeight);
+          this.draw(this.walking[0], this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height);
           break;
 
         case "ArrowLeft": //left
-          playerFrameY = 1; //set left
+          this.frameY = 1; //set left
 
-          if(playerX >= 0)
-            playerX -= playerSpeed;
-            if(playerFrameX<7)
-              playerFrameX++;
-            else playerFrameX = 0;
+          if(this.x >= 0)
+            this.x -= this.speed;
+            if(this.frameX < 7)
+              this.frameX++;
+            else this.frameX = 0;
 
-          drawSprite(images.player, playerWidth * playerFrameX, playerHeight * playerFrameY, playerWidth, playerHeight, playerX, playerY, playerWidth, playerHeight);
+          this.draw(this.walking[0], this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height);
           break;
 
         case "ArrowRight": //right
-          playerFrameY = 0; //set left
+          this.frameY = 0; //set left
 
-          if(playerX < canvas.width - playerWidth)
-            playerX += playerSpeed;
-            if(playerFrameX<7)
-              playerFrameX++;
-            else playerFrameX = 0;
+          if(this.x < canvas.width - this.width)
+            this.x += this.speed;
+            if(this.frameX < 7)
+              this.frameX++;
+            else this.frameX = 0;
 
-          drawSprite(images.player_right, playerWidth * playerFrameX, playerHeight * playerFrameY, playerWidth, playerHeight, playerX, playerY, playerWidth, playerHeight);
+          if(this.walkSource.includes('link')){
+            this.draw(this.walking[1], this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height);
+          }
+          else {
+            this.draw(this.walking[0], this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height);
+          }
           break;
+    }
   }
 }
 
+let link = new Character('36.4','32', 0, 0, 200, 100, 2, 'characters/link/walking_master.png');
+
 // On startup, show player on screen
-window.addEventListener("load", function(){
-  drawSprite(images.player, playerWidth * playerFrameX, playerHeight * playerFrameY, playerWidth, playerHeight, playerX, playerY, playerWidth, playerHeight)
+window.addEventListener('load', function(){
+  link.animate(window.event)
 });
 
 // On keystroke, perform player action
-document.addEventListener('keydown', animate_player);
+document.addEventListener('keydown', function(event) {
+  link.animate(event)
+});
 
 // On window resize, update canvas dimensions and maintain player position
 window.addEventListener('resize', function(){
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
-  drawSprite(images.player, playerWidth * playerFrameX, playerHeight * playerFrameY, playerWidth, playerHeight, playerX, playerY, playerWidth, playerHeight)
+
+  link.animate(window.event)
 })
